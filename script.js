b5097169e6bb3600031ff22736b4c504a156ca49
Antonin_Fauvet <fauvet.antonin@gmail.com>
2026-06-19 15:32:25 +0200

    Séparer le nom et le prénom dans la section identité, ajouter des identifiants aux boutons et ajuster le style des titres pour un affichage en ligne.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// fonctionnement du toggle
+// bouton toggle
 
 const toggle = document.getElementById("switch");
 const html = document.documentElement;
@@ -44,7 +44,7 @@ function changerTheme () {
     }
 }
 
-function ecouterChangementOS(e) {
+function ecouterChangementOS (e) {
     if (localStorage.getItem("theme")) {
         return;
     }
@@ -59,7 +59,7 @@ function ecouterChangementOS(e) {
     }
 }
 
-changerTheme();
+changerTheme ();
 
 toggle.addEventListener("change", darkMode);
 
@@ -67,14 +67,14 @@ mediaQueryOS.addEventListener("change", ecouterChangementOS);
 
 /*
 Ce bloque a pour objectif de modifier par programmation les éléments HTML
-afin de ne pas afficher d'information en claire. Vous pouvez soit le
+afin de ne pas afficher d'informations personnelles en claire. Vous pouvez soit le
 supprimer et modifier manuellement le HTML ;
 soit créer un dictionnaire comme celui-ci dans un fichier config.js à la
 racine du dossier carte_de_visite_digitale :
 
 const dicInfos = {
-    identite : {nom : "Nom", photo : "img/portrait_professionnel.png", role : "métier"},
-    com : {tel_txt : "01 02 03 04 05", tel_href : "tel:+330102030405", mail_txt : "adresse_mail", mail_href : "mailto:adresse_mail",
+    identite : {nom : "Nom", prenom : "Prénom", photo : "img/portrait_professionnel.png", role : "métier"},
+    com : {tel : "tel:+330102030405", mail : "mailto:adresse_mail",
         reseaux : {github : "https://github.com/pseudo_github", linkedin : "lien vers votre compte linkedin"}}
     }
 */
@@ -82,6 +82,7 @@ const dicInfos = {
 if (typeof dicInfos != 'undefined') {
     
     const nom = document.getElementById("nom");
+    const prenom = document.getElementById("prenom");
     const role = document.getElementById("role");
     const tel = document.getElementById("tel");
     const mail = document.getElementById("mail");
@@ -90,6 +91,7 @@ if (typeof dicInfos != 'undefined') {
     const photo = document.getElementById("photo");
 
     if (nom) nom.textContent = dicInfos.identite.nom;
+    if (prenom) prenom.textContent = dicInfos.identite.prenom;
     if (role) role.textContent = dicInfos.identite.role;
     if (photo) photo.src = dicInfos.identite.photo;
     if (tel) {
@@ -101,3 +103,46 @@ if (typeof dicInfos != 'undefined') {
     if (github) github.setAttribute("href", dicInfos.com.reseaux.github);
     if (linkedin) linkedin.setAttribute("href", dicInfos.com.reseaux.linkedin);
 }
+
+// bouton vCard, "Partager"
+
+function capitaliser (mot) {
+    return mot.slice(0,1).toUpperCase() + mot.slice(1).toLowerCase();
+}
+
+function genererVCard () {
+
+    const FN = capitaliser(dicInfos.identite.nom) + " " + capitaliser(dicInfos.identite.prenom);
+    const TEL = dicInfos.com.tel;
+    const EMAIL = dicInfos.com.mail;
+    const X_SOCIALPROFILE_G = dicInfos.com.reseaux.github;
+    const X_SOCIALPROFILE_L = dicInfos.com.reseaux.linkedin;
+
+    return `BEGIN:VCARD\r\nVERSION:3.0\r\nFN:${FN}\r\nTEL;TYPE=CELL:${TEL}\r\nEMAIL:${EMAIL}\r\nX-SOCIALPROFILE;TYPE=GitHub:${X_SOCIALPROFILE_G}\r\nX-SOCIALPROFILE;TYPE=LinkedIn:${X_SOCIALPROFILE_L}\r\nEND:VCARD`;
+}
+
+function genererBlobVCard() {
+
+    const vcardTexte = genererVCard();
+    const blob = new Blob([vcardTexte], { type: 'text/vcard;charset=utf-8;' });
+
+    return blob;
+}
+
+const boutonAjout = document.getElementById('ajouter');
+
+boutonAjout.addEventListener('click', function() {
+    const blob = genererBlobVCard();
+    
+    const urlBlob = URL.createObjectURL(blob);
+    
+    const lienTemporaire = document.createElement('a');
+    lienTemporaire.href = urlBlob;
+    lienTemporaire.download = `contact.vcf`;
+    
+    document.body.appendChild(lienTemporaire);
+    lienTemporaire.click();
+    
+    document.body.removeChild(lienTemporaire);
+    URL.revokeObjectURL(urlBlob);
+});
